35028fa617a8cfba0bff01c0bf27085922fbfd10
Naved Ansari <naved001@gmail.com>
2025-11-12 14:47:15 -0500

    Add tests for merge.py
    Add tests for some of the important functions in merge.py.
    
    Since I wrote the tests for pytest, I switched to using pytest as the
    runner for the rest of the tests.

---
+++ openshift_metrics/tests/conftest.py
@@ -0,0 +1,127 @@
+import pytest
+
+
+@pytest.fixture
+def mock_metrics_file1():
+    cpu_metrics = [
+        {
+            "metric": {
+                "pod": "pod1",
+                "namespace": "namespace1",
+                "resource": "cpu",
+            },
+            "values": [
+                [0, 10],
+                [60, 15],
+                [120, 20],
+            ],
+        },
+        {
+            "metric": {
+                "pod": "pod2",
+                "namespace": "namespace1",
+                "resource": "cpu",
+            },
+            "values": [
+                [0, 30],
+                [60, 35],
+                [120, 40],
+            ],
+        },
+    ]
+    memory_metrics = [
+        {
+            "metric": {
+                "pod": "pod1",
+                "namespace": "namespace1",
+                "resource": "memory",
+            },
+            "values": [
+                [0, 10],
+                [60, 15],
+                [120, 20],
+            ],
+        },
+        {
+            "metric": {
+                "pod": "pod2",
+                "namespace": "namespace1",
+                "resource": "cpu",
+            },
+            "values": [
+                [0, 30],
+                [60, 35],
+                [120, 40],
+            ],
+        },
+    ]
+    return {
+        "cluster_name": "ocp-prod",
+        "start_date": "2025-09-20",
+        "end_date": "2025-09-20",
+        "cpu_metrics": cpu_metrics,
+        "memory_metrics": memory_metrics,
+    }
+
+
+@pytest.fixture
+def mock_metrics_file2():
+    cpu_metrics = [
+        {
+            "metric": {
+                "pod": "pod1",
+                "namespace": "namespace1",
+                "resource": "cpu",
+            },
+            "values": [
+                [180, 10],
+                [240, 15],
+                [300, 20],
+            ],
+        },
+        {
+            "metric": {
+                "pod": "pod2",
+                "namespace": "namespace1",
+                "resource": "cpu",
+            },
+            "values": [
+                [180, 30],
+                [240, 35],
+                [300, 40],
+            ],
+        },
+    ]
+    memory_metrics = [
+        {
+            "metric": {
+                "pod": "pod1",
+                "namespace": "namespace1",
+                "resource": "memory",
+            },
+            "values": [
+                [180, 10],
+                [240, 15],
+                [300, 20],
+            ],
+        },
+        {
+            "metric": {
+                "pod": "pod2",
+                "namespace": "namespace1",
+                "resource": "cpu",
+            },
+            "values": [
+                [180, 30],
+                [240, 35],
+                [300, 40],
+            ],
+        },
+    ]
+    return {
+        "cluster_name": "ocp-prod",
+        "start_date": "2025-09-21",
+        "end_date": "2025-09-21",
+        "cpu_metrics": cpu_metrics,
+        "memory_metrics": memory_metrics,
+    }
---
+++ openshift_metrics/tests/test_merge.py
@@ -0,0 +1,98 @@
+import pytest
+import json
+from decimal import Decimal
+
+from openshift_metrics.merge import (
+    compare_dates,
+    get_su_definitions,
+    load_and_merge_metrics,
+    load_metadata,
+)
+
+
+@pytest.mark.parametrize(
+    "date1, date2, expected_result",
+    [
+        ("2025-01-18", "2025-01-20", True),
+        ("2025-01-18", "2025-01-16", False),
+        ("2025-01-18", "2025-01-18", False),
+    ],
+)
+def test_compare_dates(date1, date2, expected_result):
+    assert compare_dates(date1, date2) is expected_result
+
+
+def test_get_su_definitions(mocker):
+    mock_rates = {
+        "vCPUs in GPUV100 SU": Decimal("20"),
+        "RAM in GPUV100 SU": Decimal("8192"),
+        "GPUs in GPUV100 SU": Decimal("1"),
+        "vCPUs in CPU SU": Decimal("5"),
+        "RAM in CPU SU": Decimal("1024"),
+        "GPUs in CPU SU": Decimal("0"),
+    }
+    mock_rates_data = mocker.MagicMock()
+
+    def mock_get_value_at(key, month, value_type):
+        return mock_rates.get(key, Decimal("67"))
+
+    mock_rates_data.get_value_at.side_effect = mock_get_value_at
+    mocker.patch(
+        "openshift_metrics.merge.rates.load_from_url", return_value=mock_rates_data
+    )
+    report_month = "2025-10"
+    su_definitions = get_su_definitions(report_month)
+
+    assert "OpenShift GPUV100" in su_definitions
+    assert su_definitions["OpenShift GPUV100"]["vCPUs"] == Decimal("20")
+    assert su_definitions["OpenShift GPUV100"]["RAM"] == Decimal("8192")
+    assert su_definitions["OpenShift GPUV100"]["GPUs"] == Decimal("1")
+
+    assert "OpenShift CPU" in su_definitions
+    assert su_definitions["OpenShift CPU"]["vCPUs"] == Decimal("5")
+    assert su_definitions["OpenShift CPU"]["RAM"] == Decimal("1024")
+    assert su_definitions["OpenShift CPU"]["GPUs"] == Decimal("0")
+
+    # This should get the default test value
+    assert su_definitions["OpenShift GPUH100"]["GPUs"] == Decimal("67")
+
+
+def test_load_and_merge_data(tmp_path, mock_metrics_file1, mock_metrics_file2):
+    """
+    Test that we can load metrics from the 2 files and merge the metrics from those.
+
+    Note that we already have tests that test the merging of the data, this mostly
+    focuses on the loading part.
+    """
+    p1 = tmp_path / "file1.json"
+    p2 = tmp_path / "file2.json"
+
+    p1.write_text(json.dumps(mock_metrics_file1))
+    p2.write_text(json.dumps(mock_metrics_file2))
+
+    processor = load_and_merge_metrics([p1, p2])
+
+    pod1_metrics = processor.merged_data["namespace1"]["pod1"]["metrics"]
+
+    # check values from file1.json are in the merged_data
+    assert 60 in pod1_metrics  # 60 is the epoch time stamp
+    assert pod1_metrics[60]["cpu_request"] == 15
+    assert pod1_metrics[60]["memory_request"] == 15
+
+    # check values from file2.json are in the merged_data
+    assert 180 in pod1_metrics
+    assert pod1_metrics[180]["cpu_request"] == 10
+    assert pod1_metrics[180]["memory_request"] == 10
+
+
+def test_load_metadata(tmp_path, mock_metrics_file1, mock_metrics_file2):
+    """Test we can load metadata from the metrics files."""
+
+    p1 = tmp_path / "file1.json"
+    p2 = tmp_path / "file2.json"
+    p1.write_text(json.dumps(mock_metrics_file1))
+    p2.write_text(json.dumps(mock_metrics_file2))
+    metadata = load_metadata([p1, p2])
+    assert metadata.cluster_name == "ocp-prod"
+    assert metadata.report_start_date == "2025-09-20"
+    assert metadata.report_end_date == "2025-09-21"
